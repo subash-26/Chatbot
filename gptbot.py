a04b1959c29306db5f1513c6b44e29c88135310a
rider0 <subashtherider0>
2024-05-13 21:12:01 +0530

    audio changes

--- gptbot.py
+++ gptbot.py
@@ -1,7 +1,13 @@
 import openai 
 import streamlit as st
 import pydantic
+import os
+from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import *
 
+# Float feature initialization
+float_init()
 
 st.title("Chat-GPT like clone")
 
@@ -52,24 +58,42 @@ if prompt := st.chat_input("Whats Up ?"):
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
-    #Response Message
-#   with st.chat_message("assistant"):
-#        message_placeholder = st.empty()
-#        full_response = ""
-#        for response in openai.chat.completions.create(
-#            model=st.session_state["openai_model"],
-#            messages=[
-#                {"role": m["role"], "content": m["content"]}
-#                for m in st.session_state.messages
-#            ],
-#            stream = True,
-#        ):
-            #full_response += response.choices[0].delta.get("content", "")
-#            full_response += response.choices[0].
-#            message_placeholder.markdown(full_response + " ")
-#       message_placeholder.markdown(full_response) 
-    
+# Create footer container for the microphone
+footer_container = st.container()
+with footer_container:
+    audio_bytes = audio_recorder()
 
-#    st.session_state.messages.append({"role" : "assistant", "content" : response})'''
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if audio_bytes:
+    # Write the audio bytes to a file
+    with st.spinner("Transcribing..."):
+        webm_file_path = "temp_audio.mp3"
+        with open(webm_file_path, "wb") as f:
+            f.write(audio_bytes)
+
+        transcript = speech_to_text(webm_file_path)
+        if transcript:
+            st.session_state.messages.append({"role": "user", "content": transcript})
+            with st.chat_message("user"):
+                st.write(transcript)
+            os.remove(webm_file_path)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking🤔..."):
+            final_response = get_answer(st.session_state.messages)
+        with st.spinner("Generating audio response..."):    
+            audio_file = text_to_speech(final_response)
+            autoplay_audio(audio_file)
+        st.write(final_response)
+        st.session_state.messages.append({"role": "assistant", "content": final_response})
+        os.remove(audio_file)
+
+# Float the footer container and provide CSS to target it with
+footer_container.float("bottom: 0rem;")
                                      
                     
